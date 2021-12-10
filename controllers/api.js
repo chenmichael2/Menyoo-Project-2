29eff1923b8b942bbad452a408d679b13795205b
chenmichael2 <chenmichael2012@gmail.com>
2021-12-09 17:01:48 -0800

    trying api

--- controllers/api.js
+++ controllers/api.js
@@ -7,6 +7,7 @@ const { Op } = require("sequelize");
 const fetch = require("node-fetch");
 let API_URL = "https://api.yelp.com/v3/businesses/{id}";
 const yelp = require('yelp-fusion');
+const axios = require('axios');
 
 
 const apiKey = 'SwZeP7pkzyUv8lnN-mLpMCnzHLDA3fP1X0YkxiZv3pXSkvULcBOMtKCy3buty5aVbtyBr9FesRUZxIeDUk__DUdw0FgQkHB9mELZ_HBXQr8MpIhAFaFdKXwW8UqxYXYx';
@@ -15,16 +16,20 @@ const client = yelp.client(apiKey);
 const { restaurant } = require('../models');
 
 //API
+
 router.get('/', function (req, res) {
-    console.log('getting API');
-    client.search()
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(e) {
-        console.log(e);
-      });
-    fetchApi();
+  response = axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${API_KEY}&number=${queryResultsQuantity}`)
+
+
+    // console.log('getting API');
+    // client.search()
+    //   .then(function(response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function(e) {
+    //     console.log(e);
+    //   });
+    // fetchApi();
 })
 
 function fetchApi() {
